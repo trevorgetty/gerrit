dfcfecd9b3d5c4d8979e424f804b22deb49a0c75
Ronan <ronan.conway@wandisco.com>
2021-11-05 15:18:38 +0000

    GER-1482: Lowering log message level to INFO
    log message being lowered to INFO level
    
    + Some customers use the replication plugin and
      will trigger this message a lot. It has been
      decided that the log message should be at
      INFO level due to this.
    
    Change-Id: I5207c933a7973e93a20d019c952b49b2f8f7ba2e

--- gerrit-server/src/main/java/com/google/gerrit/common/replication/PersistedEventInformation.java
+++ gerrit-server/src/main/java/com/google/gerrit/common/replication/PersistedEventInformation.java
@@ -297,7 +297,8 @@ public class PersistedEventInformation {
     // projectName member
     // then we should generate an error. The default sha1 will be used.
     if (originalEvent.getProjectName() == null) {
-      logger.atWarning().log("The following Event Type %s has a Null project name. "
+      //This message is logged at INFO level due to the replication plugin causing at lot of spam on the dashboard.
+      logger.atInfo().log("The following Event Type %s has a Null project name. "
               + "Unable to set the event filename using the sha1 of the project name. "
               + "Using All-Projects as the default project, and updating the event data to match",
           originalEvent.getEvent());
